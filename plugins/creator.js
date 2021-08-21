a9e77620f2f5ac889899ad914cb88cc38b339f4c
destioas <84615256+destioas@users.noreply.github.com>
2021-08-21 20:24:08 +0700

    creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, '6288709645535', this.getName('6288709645535@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '628980361858', this.getName('6288709645535@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
